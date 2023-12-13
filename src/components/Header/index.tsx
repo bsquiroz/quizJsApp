5098131c2d79bb4357beea589aae5fed83cd685a
bsquiroz <bemq1605@gmail.com>
2023-12-12 19:27:47 -0500

    add button reset while aswer quiz

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -17,37 +17,33 @@ export const Header = () => {
 			alignItems={"center"}
 			justifyContent={"center"}
 			gap={5}
+			position={"relative"}
+			borderRadius={"1rem"}
+			overflow={"hidden"}
+			padding={"0.5rem"}
 		>
 			<div
 				style={{
-					padding: "0.5rem",
-					borderRadius: "1rem",
-					position: "relative",
-					textAlign: "center",
-					overflow: "hidden",
+					backgroundColor: questions.length
+						? "#efda4d"
+						: "transparent",
+					content: "",
+					position: "absolute",
+					height: "100%",
+					width: questions.length ? `${value}%` : "100%",
+					left: 0,
+					top: 0,
 				}}
-			>
-				<div style={{ position: "relative", zIndex: 10 }}>
-					<Time />
-					<h1>Javascript quiz</h1>
-				</div>
-				<div
-					style={{
-						backgroundColor: questions.length
-							? "#efda4d"
-							: "transparent",
-						content: "",
-						position: "absolute",
-						height: "100%",
-						width: questions.length ? `${value}%` : "100%",
-						left: 0,
-						top: 0,
-					}}
-				></div>
+			></div>
+			<div style={{ position: "relative", zIndex: 10 }}>
+				<Time />
+				<h1>Javascript quiz</h1>
 			</div>
-			<figure>
-				<img src="/images/javascript.png" alt="image js" />
-			</figure>
+			<img
+				style={{ position: "relative", zIndex: 10 }}
+				src="/images/javascript.png"
+				alt="image js"
+			/>
 		</Stack>
 	);
 };
--- src/components/QuestionIndicator/index.tsx
+++ src/components/QuestionIndicator/index.tsx
@@ -1,5 +1,5 @@
 import { useQuestionStore } from "../../store/questions";
-import { Stack, IconButton } from "@mui/material";
+import { Stack, IconButton, Button } from "@mui/material";
 import { ArrowBack, ArrowForward } from "@mui/icons-material";
 
 export const QuestionIndicator = () => {
@@ -9,27 +9,34 @@ export const QuestionIndicator = () => {
 	const goPreviousQuestion = useQuestionStore(
 		(state) => state.goPreviousQuestion
 	);
+	const reset = useQuestionStore((state) => state.reset);
 
 	return (
-		<Stack
-			direction={"row"}
-			gap={2}
-			alignItems={"center"}
-			justifyContent={"center"}
-		>
-			<IconButton
-				onClick={goPreviousQuestion}
-				disabled={currentQuestion === 0}
+		<Stack gap={1}>
+			<Stack
+				direction={"row"}
+				gap={2}
+				alignItems={"center"}
+				justifyContent={"center"}
 			>
-				<ArrowBack />
-			</IconButton>
-			{currentQuestion + 1} / {questions.length}
-			<IconButton
-				onClick={goNextQuestion}
-				disabled={currentQuestion === questions.length - 1}
-			>
-				<ArrowForward />
-			</IconButton>
+				<IconButton
+					onClick={goPreviousQuestion}
+					disabled={currentQuestion === 0}
+				>
+					<ArrowBack />
+				</IconButton>
+				{currentQuestion + 1} / {questions.length}
+				<IconButton
+					onClick={goNextQuestion}
+					disabled={currentQuestion === questions.length - 1}
+				>
+					<ArrowForward />
+				</IconButton>
+			</Stack>
+
+			<Button variant="outlined" color="error" onClick={reset}>
+				Cancelar quiz y volver al menu
+			</Button>
 		</Stack>
 	);
 };
